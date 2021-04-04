9a04c0bb4eca6413c49bcaf12b2534b64e648c79
KQTENQK <67883180+KQTENQK@users.noreply.github.com>
2021-04-04 06:01:51 +0300

    Init

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private float _sliderRate;
+
+    private Slider _slider;
+
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    private void OnEnable()
+    {
+        _player.ValueChanged += ChangeValues;
+    }
+
+    private void OnDisable()
+    {
+        _player.ValueChanged -= ChangeValues;
+    }
+
+    private void ChangeValues()
+    {
+        float currentPlayerHealth = _player.Health * 0.01f;
+        float currentSliderValue = _slider.value;
+        StartCoroutine(MoveValue(currentPlayerHealth, currentSliderValue));
+    }
+
+    private IEnumerator MoveValue(float currentPlayerHealth, float currentSliderValue)
+    {
+        while (currentSliderValue != currentPlayerHealth)
+        {
+            currentSliderValue = Mathf.MoveTowards(currentSliderValue, currentPlayerHealth, Time.deltaTime * _sliderRate);
+            _slider.value = currentSliderValue;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public event UnityAction ValueChanged;
+
+    [SerializeField] private float _hitButtonDamage;
+    [SerializeField] private float _healButtonHeal;
+    [SerializeField] private Button _hitButton;
+    [SerializeField] private Button _healButton;
+
+    public float Health { get; private set; }
+
+    private void Start()
+    {
+        Health = 100;
+    }
+
+    private void OnEnable()
+    {
+        _hitButton.onClick.AddListener(TakeDamage);
+        _healButton.onClick.AddListener(TakeHeal);
+    }
+
+    private void OnDisable()
+    {
+        _hitButton.onClick.RemoveListener(TakeDamage);
+        _healButton.onClick.RemoveListener(TakeHeal);
+    }
+
+    private void TakeDamage()
+    {
+        Health -= _hitButtonDamage;
+        ValueChanged?.Invoke();
+    }
+
+    private void TakeHeal()
+    {
+        Health += _healButtonHeal;
+        ValueChanged?.Invoke();
+    }
+}
